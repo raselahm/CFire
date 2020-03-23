f798cfe2f781704702b8051604f476bf5eea4ef3
raselahm <32113495+raselahm@users.noreply.github.com>
2020-03-23 16:25:15 -0400

    made makepost file/ test commit

---
+++ makepost/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Createpost from "./Component/Createpost";
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Createpost/>
+    );
+  }
+}
+
+export default App;
