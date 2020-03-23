9c1d0e4f6985f33bd3d9f6605adf22e22154c393
raselahm <32113495+raselahm@users.noreply.github.com>
2020-03-23 17:19:53 -0400

    Task #45
    Added navbar made by teammate

--- makepost/src/Component/Createpost.js
+++ makepost/src/Component/Createpost.js
@@ -1,1 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
 
+class Createpost extends React.Component {
+
+    render() {
+      return (
+        <div className="row Homepage">
+        <header className="col-12 Home-Header">
+          <Navbar/> 
+        </header>
+        </div>
+      );
+    }
+  }
+  
+  export default Createpost;
