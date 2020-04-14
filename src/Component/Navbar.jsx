0d0e4801dcddc10ff770fa2565a544d99afbbf8c
Jay  Jeffries <jewan.jeffries@gmail.com>
2020-04-14 08:47:26 -0400

    #73 logout button/NavBar Routing
    logout button now works properly and navBar change to route using withRouter not hyperlinks

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,30 +1,57 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import "./Navbar.css"
 
 
-export default class Navbar extends React.Component {
+ class Navbar extends Component {
+
+    toHome = e =>{
+        this.props.history.push('/Homepage');
+    }
+
+    toMessages = e =>{
+        this.props.history.push('/MessagesPage');
+    }
+
+    toProfile = e =>{
+        this.props.history.push('/Profile');
+    }
+
+    logout = e =>{
+        fetch("http://stark.cse.buffalo.edu/cse410/blackhole/api/SocialAuth.php",{
+            method: "post",
+            body: JSON.stringify({
+                action: "logout",
+                username: "jewanjef@buffalo.edu", 
+                session_token: sessionStorage.getItem("token")
+            })
+        });
+
+        this.props.history.push('/')
+
+    }
 
     render(){
 
         return(
         <div id="row navbar" className="navbar">
-            <ul id="side-menu-items">
-                <Link to = '/Homepage'><li className="nav-element">
+            <ul id="navItems">
+                <button onClick = {this.toHome} className="nav-element">
                     Home
-                </li></Link>
-                <Link to = '/MessagePage'><li className="nav-element">
+                </button>
+                <button onClick = {this.toMessages} className="nav-element">
                     Messages
-                </li></Link>
-                <Link to = '/ProfilePage'><li className="nav-element">
+                </button>
+                <button onClick = {this.toProfile} className="nav-element">
                 Profile
-                </li></Link>
-                <Link to = '/loginPage'><li className="nav-element">
+                </button>
+                <button onClick = {this.logout} className="nav-element">
                 Log Out
-                </li></Link>
+                </button>
             </ul>
         </div>
         );
             }
 
 }
+export default withRouter(Navbar)
